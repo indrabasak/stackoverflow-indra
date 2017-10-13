f02c73a2dea4a44dbfdde36f9537609a364b4c24
Indra Basak <indra.basak1@gmail.com>
2017-10-13 14:20:46 -0700

    Added an example in spring-test-examples project.

--- spring-test-examples/src/main/java/com/basaki/controller/BookController.java
+++ spring-test-examples/src/main/java/com/basaki/controller/BookController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
 
 /**
  * {@code BookController} is the spring REST controller for book API.
